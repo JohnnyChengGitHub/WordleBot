c158501641f69cc79df32a904fe2e7b3876d7706
Jeff <jeff4elee@gmail.com>
2022-02-09 18:26:47 -0800

    Adding scaffolding and tests

---
+++ solver.py
@@ -0,0 +1,13 @@
+import file_reader
+
+def get_pattern_from_guess(guess: str, hidden_word: str) -> str:
+    """
+    Given a guess and a hidden word, this function
+    should output a hint. The hint in this case is
+    a 5 letter string, where each letter is either
+    'G', 'Y', or 'B'
+
+    e.g. given the guess 'hello' for hidden word 'whale'
+    the return value should be 'YYBGB'
+    """
+    return NotImplementedError
