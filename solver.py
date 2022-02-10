7738bbfe0602e2346c947d3f804cd091f576cc29
Michelle <yang.michelle@ucla.edu>
2022-02-09 22:46:56 -0800

    Michelle's first rendition of solver

--- solver.py
+++ solver.py
@@ -1,6 +1,11 @@
 import file_reader
 
-def get_pattern_from_guess(guess: str, hidden_word: str) -> str:
+def get_pattern_from_guess(guess: str, hidden_word:str) -> str:
+    pattern = ["X","X","X","X","X"] 
+    green = (reveal_green(guess, hidden_word, pattern))
+    black = (reveal_black(guess, hidden_word, green))
+    yellow = (reveal_yellow(guess, hidden_word, black))
+    return "". join(yellow)
     """
     Given a guess and a hidden word, this function
     should output a hint. The hint in this case is
@@ -10,4 +15,28 @@ def get_pattern_from_guess(guess: str, hidden_word: str) -> str:
     e.g. given the guess 'hello' for hidden word 'whale'
     the return value should be 'YYBGB'
     """
-    return NotImplementedError
+def reveal_green(guess, hidden_word, pattern):
+    for i in range(len(guess)):
+        char_a = guess[i]
+        char_b = hidden_word[i]
+        if char_a == char_b:
+            pattern[i] = "G"
+    return pattern
+def reveal_black (guess,hidden_word, pattern):
+    for i in range(len(guess)):
+        char_a = guess[i]
+        char_b = hidden_word[i]
+        if char_a != char_b:
+            pattern[i] = "B"
+    return pattern
+def reveal_yellow(guess, hidden_word, pattern):
+    for i in range(len(guess)):
+        char_a = guess[i]
+        for j in range (len(guess)):
+            char_b = hidden_word[j]
+            if (char_a == char_b and i != j):
+                pattern [i] = "Y"
+    return pattern
+
+
+#     # return NotImplementedError
